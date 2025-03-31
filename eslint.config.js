0a0c2d1d88601aeff55951aa1a460ee1090333c8
LekoArts <lekoarts@gmail.com>
2025-03-31 19:38:11 +0200

    linting

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+	stylistic: {
+		indent: 'tab',
+		quotes: 'single',
+		semi: false,
+	},
+	formatters: true,
+	astro: true,
+	typescript: true,
+})
