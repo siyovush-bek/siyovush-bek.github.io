3399a4e5add4510ed0cdcbdb7751d6a4a798e5c8
LekoArts <lekoarts@gmail.com>
2025-06-23 10:04:30 +0200

    feat: Add autolink headings

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,11 @@ import tailwindcss from '@tailwindcss/vite'
 import { imageService } from '@unpic/astro/service'
 import expressiveCode from 'astro-expressive-code'
 import { defineConfig, fontProviders } from 'astro/config'
-import rehypeExternalLinks from 'rehype-external-links'
+import { toString } from 'hast-util-to-string'
+import { h, s } from 'hastscript'
+import rehypeAutolinkHeadings, { type Options as AutolinkHeadingsOptions } from 'rehype-autolink-headings'
+import rehypeExternalLinks, { type Options as ExternalLinkOptions } from 'rehype-external-links'
+import rehypeSlug from 'rehype-slug'
 import remarkDirective from 'remark-directive'
 import remarkSmartypants from 'remark-smartypants'
 import { SITE } from './src/constants'
@@ -42,6 +46,7 @@ export default defineConfig({
 		// @ts-expect-error: Astro types don't match remark plugin
 		remarkPlugins: [[remarkSmartypants, { backticks: false }], remarkDirective, remarkAsides, [remarkSandpack, { componentName: ['Playground'] }]],
 		rehypePlugins: [
+			rehypeSlug,
 			[
 				rehypeExternalLinks,
 				{
@@ -50,7 +55,27 @@ export default defineConfig({
 					content: { type: 'text', value: ' (opens in a new window)' },
 					properties: { className: ['external_link'] },
 					contentProperties: { className: ['sr-only'] },
-				},
+				} satisfies ExternalLinkOptions,
+			],
+			[
+				rehypeAutolinkHeadings,
+				{
+					behavior: 'after',
+					group() {
+						return h('.markdown-heading')
+					},
+					headingProperties() {
+						return { tabIndex: -1 }
+					},
+					properties(node: any) {
+						return { ariaLabel: `Permalink: ${toString(node)}`, className: 'anchor' }
+					},
+					content() {
+						return h('svg', { className: 'anchor-icon', viewBox: '0 0 16 16', ariaHidden: true }, [
+							s('path', { d: 'm7.775 3.275 1.25-1.25a3.5 3.5 0 1 1 4.95 4.95l-2.5 2.5a3.5 3.5 0 0 1-4.95 0 .751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018 1.998 1.998 0 0 0 2.83 0l2.5-2.5a2.002 2.002 0 0 0-2.83-2.83l-1.25 1.25a.751.751 0 0 1-1.042-.018.751.751 0 0 1-.018-1.042Zm-4.69 9.64a1.998 1.998 0 0 0 2.83 0l1.25-1.25a.751.751 0 0 1 1.042.018.751.751 0 0 1 .018 1.042l-1.25 1.25a3.5 3.5 0 1 1-4.95-4.95l2.5-2.5a3.5 3.5 0 0 1 4.95 0 .751.751 0 0 1-.018 1.042.751.751 0 0 1-1.042.018 1.998 1.998 0 0 0-2.83 0l-2.5 2.5a1.998 1.998 0 0 0 0 2.83Z' }),
+						])
+					},
+				} satisfies AutolinkHeadingsOptions,
 			],
 		],
 	},
